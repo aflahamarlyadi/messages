5de77edf5688667b6e944ddfeaa9d7cf20250436
Aflah Hanif Amarlyadi <aflahamarlyadi@gmail.com>
2025-09-16 14:28:33 +0700

    Add User and Contact types

---
+++ types/Contact.ts
@@ -0,0 +1,6 @@
+export interface Contact {
+  phoneNumber: string;
+  name: string;
+  id: string | null;
+  profilePicture: string | null;
+};
---
+++ types/User.ts
@@ -0,0 +1,7 @@
+export interface User {
+  phoneNumber: string;
+  createdAt: Date;
+  name: string | null;
+  email: string | null;
+  profilePicture: string | null;
+};
